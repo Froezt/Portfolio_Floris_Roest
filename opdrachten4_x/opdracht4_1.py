e23efb9625031fa46d8dad42d98802c13dd0fd93
Froezt <36000801+Froezt@users.noreply.github.com>
2018-01-31 19:41:00 +0100

    Delete opdracht4_1.py

--- opdrachten4_x/opdracht4_1.py
+++
@@ -1,7 +0,0 @@
-def som(getal1, getal2, getal3):
-    return getal1 + getal2 + getal3
-    resultaat = getal1+getal2+getal3
-getal1 = int(input('Wat is het eerste getal: '))
-getal2 = int(input('Wat is het tweede getal: '))
-getal3 = int(input('Wat is het derde getal: '))
-print (som(getal1, getal2, getal3))
