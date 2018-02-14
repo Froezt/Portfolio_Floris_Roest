e363c36e24a5238b38642c68e48bf71a306cb0de
Froezt <36000801+Froezt@users.noreply.github.com>
2018-02-14 10:42:04 +0100

    Update Final Assignment XML stationlijsten.py

--- opdrachten10_x/Final Assignment XML stationlijsten.py
+++ opdrachten10_x/Final Assignment XML stationlijsten.py
@@ -1,26 +1,28 @@
 import xmltodict
+
 def processXML(filename):
-    with open (filename) as myXMLfile:
-        filestring = myXMLfile.read()
-        xmldictionary = xmltodict.parse(filestring)
+    with open(filename) as myXMLFile:
+        filecontentstring = myXMLFile.read()
+        xmldictionary = xmltodict.parse(filecontentstring)
         return xmldictionary
-stationsdict = processXML('Stations.xml')
-stations = stationsdict ['Stations']['Station']
-print('Dit zijn de codes en types van de 4 stations: ')
-for station in stations:
-    code = station['Code']
-    type = station['Type']
-    print('{:4} - {}'.format(code, type))
 
-print('Dit zijn alle stations met 1 of meer synoniemen: ')
+stationsdict = processXML('fa.xml')
+stations = stationsdict['Stations']['Station']
+
+print('Dit zijn de codes en types van de 4 stations:')
+
 for station in stations:
-    code = station['Code']
-    synoniemen = station['Synoniemen']
+    print('{:4} - {}'.format(station['Code'], station['Type']))
+
+print('')
+print('Dit zijn alle stations met één of meer synoniemen:')
+
+for station in stations:
     if station['Synoniemen'] is not None:
-        print('{:4} - {}'.format(code, synoniemen))
+        print(station['Synoniemen'])
 
-print('Dit is de lange naam van elk station: ')
+print('')
+print('Dit is de lange naam van elk station:')
+
 for station in stations:
-    code = station['Code']
-    lange_naam = station['Namen']['Lang']
-    print('{:4} - {}'.format(code, lange_naam))
+    print('{:4} - {}'.format(station['Code'], station['Namen']['Lang']))
