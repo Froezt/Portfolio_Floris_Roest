9e4ebd0b0d2f2cb26a138a0a199ec02c45459200
Froezt <36000801+Froezt@users.noreply.github.com>
2018-02-14 13:11:33 +0100

    Update opdracht6_2.py

--- opdrachten6_x/opdracht6_2.py
+++ opdrachten6_x/opdracht6_2.py
@@ -2,5 +2,5 @@
 eval(input('Geef een lijst met minimaal 10 strings: '))
 lijst = ['boter', 'kaas', 'bier', 'pizza', 'thee', 'drop', 'koek', 'cola', 'boterham', 'stampot']
 for product in lijst:
-    if str(4):
+    if length(product) == 4:
         print(lijst)
